7ff5e454266c285301a2bb7c6bceca2feb27440a
a377455-hub <a377455@uach.mx>
2026-09-15 08:39:31 -0700

    Update temperatura.py

--- temperatura.py
+++ temperatura.py
@@ -4,10 +4,9 @@ st.title ("Clasificador de temperatura")
 
 temperatura = st.number_input(20)
 "Introduce la temperatura en °C"
-
 if temperatura <= 10:
 print("Hace frío.")
-elif temperatura >10 and temperatura <=24:
+elif temperatura >10 and temperatura <=24
 print("La temperatura es agradable.")
 else:
 print("Hace calor.")
