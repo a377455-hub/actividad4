cd61357045473d0757411ce864f94bf6d6f27b87
a377455-hub <a377455@uach.mx>
2026-09-15 08:28:51 -0700

    Update temperatura.py

--- temperatura.py
+++ temperatura.py
@@ -1,1 +1,15 @@
 import streamlit as st 
+
+st.title ("Clasificador de temperatura")
+
+temperatura = st.number_input(
+  "Introduce la temperatura en °C
+  value = 20
+
+  if temperatura =< 10:
+  print("Hace frío.")
+  elif temperatura >10 and temperatura <=24:
+  print("La temperatura es agradable.")
+  else:
+  print("Hace calor.)
+  
