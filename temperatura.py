716070ea30e694fb4f126872768767b388b3544c
a377455-hub <a377455@uach.mx>
2026-09-15 08:30:46 -0700

    Update temperatura.py

--- temperatura.py
+++ temperatura.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title ("Clasificador de temperatura")
 
 temperatura = st.number_input(
-  "Introduce la temperatura en °C
+  "Introduce la temperatura en °C"
   value = 20
 
   if temperatura =< 10:
